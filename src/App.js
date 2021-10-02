d2d7afc265b69cd299b3afd6c0d183850ac8e8a4
aa-nadim <nadim.ice.nstu@gmail.com>
2021-10-03 00:29:38 +0600

    Practices reading by Documentation of Redux Toolkit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux'
-
-import { increment, decrement, incrementByAmount } from "./app/counterSlice";
+import React from 'react';
+import { Counter } from './features/counter/Counter';
 
 const App = () => {
-    const counter = useSelector(state => state.counter.value);
-    const dispatch = useDispatch()
-    
-    const incrementCounter = () => {
-        dispatch(increment());
-    };
-    const decrementCounter = () => {
-        dispatch(decrement());
-    };
     return (
-        <div style={{fontSize:"60px"}}>
-            <div>Counter === {counter}</div>
-            <button style={{fontSize:"50px"}} onClick={incrementCounter}>
-                increment
-            </button>
-            <button style={{fontSize:"50px"}} onClick={decrementCounter}>
-                decrement
-            </button>
+        <div>
+            <Counter />
         </div>
     );
 };
