aa5a3ee9dbbd28ac0bde1af8a641fc75b2245a3d
aa-nadim <nadim.ice.nstu@gmail.com>
2021-10-13 20:28:02 +0600

    Redux Persist

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,18 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from './counterSlice';
+
+import storage from 'redux-persist/lib/storage';
+import { persistReducer } from 'redux-persist';
+
+
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+const persistedReducer = persistReducer(persistConfig, counterReducer);
 
 export const store = configureStore({
     reducer: {
-        counter: counterReducer,
+        counter: persistedReducer,
     },
 })
--- src/index.js
+++ src/index.js
@@ -5,9 +5,15 @@ import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
+let persistor = persistStore(store);
+
 ReactDOM.render(
     <Provider store={store}>
-        <App />
-    </Provider>, 
+        <PersistGate loading={null} persistor={persistor}>
+            <App />
+        </PersistGate>
+    </Provider>,
     document.getElementById('root')
 );
